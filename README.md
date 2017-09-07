4ab5dcd5fad684eb19f43f851690cad08d3d3478
Charles Stimler <charles_st@yahoo.com>
2017-09-06 20:10:13 -0700

    Initial commit of Project 3c

---
+++ README.md
@@ -0,0 +1,13 @@
+This repo is the pr2_robot/scripts directory of the Udacity Advanced 
+Pick-n-Place project.  I am knee-deep in rewrites and trouble-shooting so
+it represents a potpourri of files that are in various stages of development.
+Nevertheless, the most important files can be demarcated which are: 
+  
+  - publishsegment.py
+  - publishsegment2.py
+
+These represent the latest and most advanced iteration of the 
+object_recogition.py like files for this project, which implements the 
+imaging pipeline including the voxel downsampling, table segmentation, etc.
+
+
---
+++ short.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import numpy as np
+import sklearn
+from sklearn.preprocessing import LabelEncoder
+
+import pickle
+
+from sensor_stick.srv import GetNormals
+from sensor_stick.features import compute_color_histograms
+from sensor_stick.features import compute_normal_histograms
+from visualization_msgs.msg import Marker
+
+from sensor_stick.marker_tools import *
+from sensor_stick.msg import DetectedObjectsArray
+from sensor_stick.msg import DetectedObject
+from sensor_stick.pcl_helper import *
+
+if __name__ == '__main__':
+
+    # TODO: ROS node initialization
+    rospy.init_node('clustering', anonymous=True)
+
+    # TODO: Create Subscribers
+    pcl_sub = rospy.Subscriber("/sensor_stick/point_cloud", pc2.PointCloud2, pcl_callback, queue_size=1)
+
+    # TODO: Create Publishers
+    # Create Publishers
+    # TODO: here you need to create two publishers
+    # Call them object_markers_pub and detected_objects_pub
+    # Have them publish to "/object_markers" and "/detected_objects" with 
+    # Message Types "Marker" and "DetectedObjectsArray" , respectively
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray)
+
+ # TODO: Spin while node is not shutdown
+    while not rospy.is_shutdown():
+     rospy.spin()
